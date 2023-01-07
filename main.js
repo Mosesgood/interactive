ba04096784ce16f579f44a358e34706c37a9f6ed
Moses Pompilus <mpompilus@outlook.com>
2023-01-07 12:50:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const container = document.querySelectorAll("container");
+const thx = document.querySelectorAll("thx");
+const submit = document.getElementById("submit");
+submit.addEventListener("click", submit1);
+
+
+function submit1() {
+    location.replace("next.html")
+}
+
+
+
---
+++ main2.js
@@ -0,0 +1,8 @@
+const container = document.querySelectorAll("container");
+const thx = document.querySelectorAll("thx");
+const submit = document.getElementById("submit");
+submit.addEventListener("click", submit2);
+
+function submit2() {
+    location.replace("index.html")
+}
